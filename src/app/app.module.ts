10ea61fc0347664e9c944a6923154f64b9ca1bcc
aartipurwaha <aarti.purwaha94@gmail.com>
2017-10-31 12:00:20 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { LeftContentComponent } from './left-content/left-content.component';
+import { RightContentComponent } from './right-content/right-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContentWrapperComponent,
+    LeftContentComponent,
+    RightContentComponent
   ],
   imports: [
     BrowserModule
